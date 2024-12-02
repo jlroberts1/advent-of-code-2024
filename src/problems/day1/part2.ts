4440d8672171bbaa79789819b482d6dca130ddd2
James Roberts <105246977+jlroberts1@users.noreply.github.com>
2024-12-01 19:58:29 -0500

    Update day1 part2

--- src/problems/day1/part2.ts
+++ src/problems/day1/part2.ts
@@ -2,8 +2,25 @@
 import input from "./input.txt?raw";
 
 export const problemLink = "https://adventofcode.com/2020/day/1#part2";
+
 export function solve() {
-  console.log(input);
-  // Your solution here
-  return 0;
+  const [list1, list2] = input
+    .split(/\s+/)
+    .map(Number)
+    .reduce<[number[], number[]]>(
+      ([arr1, arr2], num, index) => {
+        if (index % 2 === 0) {
+          arr1.push(num);
+        } else {
+          arr2.push(num);
+        }
+        return [arr1, arr2];
+      },
+      [[], []],
+    );
+
+  return list1.reduce((acc, num) => {
+    const occurrences = list2.filter((x) => x == num).length;
+    return acc + num * occurrences;
+  }, 0);
 }
